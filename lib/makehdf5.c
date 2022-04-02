29433cb6b3626c4ffe6cb56e4399a4ead28f01d2
Gord Stephen <gord.stephen@nrel.gov>
2022-04-02 11:11:10 -0700

    Move set_attribute_int to avoid implicit function declaration

--- lib/makehdf5.c
+++ lib/makehdf5.c
@@ -155,6 +155,14 @@ void add_times(hid_t meta, const char* localformat, int compressionlevel) {
 
 }
 
+void set_attribute_int(hid_t dset, char* name, size_t* value) {
+    hid_t dspace = H5Screate(H5S_SCALAR);
+    hid_t attr = H5Acreate(dset, name, H5T_NATIVE_INT, dspace, H5P_DEFAULT, H5P_DEFAULT);
+    H5Awrite(attr, H5T_NATIVE_INT, value);
+    H5Aclose(attr);
+    H5Sclose(dspace);
+}
+
 hid_t dataset(hid_t dat, struct plexosKeyIndex* ki, int compressionlevel) {
 
     struct plexosTable* phase = get_phasetype(ki->key.ptr->phase);
@@ -252,14 +260,6 @@ void add_values(hid_t dat, int compressionlevel) {
 
     }
 
-}
-
-void set_attribute_int(hid_t dset, char* name, size_t* value) {
-    hid_t dspace = H5Screate(H5S_SCALAR);
-    hid_t attr = H5Acreate(dset, name, H5T_NATIVE_INT, dspace, H5P_DEFAULT, H5P_DEFAULT);
-    H5Awrite(attr, H5T_NATIVE_INT, value);
-    H5Aclose(attr);
-    H5Sclose(dspace);
 }
 
 char default_localformat[20] = "%d/%m/%Y %T";
